440375a9174770bc63902dd39891eca6380b26cf
Gersey <gerseystelmach@gmail.com>
2022-06-16 18:13:53 +0200

    plugin creation done

--- wordpress/wp-content/plugins/wp-best-redirection/assets/js/redirection-field.js
+++ wordpress/wp-content/plugins/wp-best-redirection/assets/js/redirection-field.js
@@ -12,7 +12,7 @@ jQuery(document).ready(($) => {
             id: id
         }).then(() => {
             console.log('Success');
-            deleteBtn.parentNode.parentNode.removeChild(deleteBtn.parentNode);
+            deleteBtn.parentNode.parentNode.removeChild(deleteBtn.parentNode); // deleting dynamically the input
         }).catch(() => {
             console.log('Error');
         });
@@ -23,27 +23,3 @@ jQuery(document).ready(($) => {
         deleteBtn.addEventListener('click', () => deleteRedirection(redirectionId, deleteBtn))
     })
 });
-
-/*
-jQuery(document).ready(($) => {
-    const deleteRedirection = (id) => {
-        console.log(id);
-    }
-
-    const deleteBtnList = document.querySelectorAll('.redirection-delete').forEach((deleteBtn) => {
-        const redirectionId = deleteBtn.getAttribute('data-redirection-id');
-        deleteBtn.addEventListener('click',() => deleteRedirection(redirectionId))
-    })
-
-})
-
-$.post(wp_best_redirection_manage_field.ajax_url, {
-
-    _ajax_nonce: wp_best_redirection_manage_field.nonce,
-    action: 'wp_best_redirection_manage_field_delete',
-    id: 7
-}).then(() => {
-    console.log('Success');
-}).catch(() => {
-    console.log('Error');
-});*/
